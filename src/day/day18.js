a2f76a637b5479f99e16a2646584b024d74ebc7e
Claudio Restifo <claudio@samuiweb.net>
2019-10-24 18:04:31 +0700

    day18

--- src/day/day18.js
+++ src/day/day18.js
@@ -5,8 +5,10 @@ import CONF from "../conf.js";
 /**
  * Pipes
  *
- * initialize a new state value called `pipes` as an empty array
- *  wip
+ * 1 - initialize a new method called updatePipes that returns an array
+ * 2 - initialize a new state value called `pipes` as an empty array
+ * 3 - inside update game calls updatePipes and assign it to a variable
+ * 4 - pass it to `setState` to update pipes
  */
 
 const Bird = ({ top }) => {
@@ -45,10 +47,12 @@ class App extends React.Component {
   // Perform all our calculation first then pass the new data to setState
   updateGame = () => {
     const newFallPosition = this.fall();
+    const newPipes = this.updatePipes();
 
     this.setState({
       top: newFallPosition.newPos,
       deltaTop: newFallPosition.newDeltaPos,
+      pipes: newPipes,
     });
   }
 
@@ -90,6 +94,21 @@ class App extends React.Component {
     if(kc === 32) {
       this.jump();
     }
+  }
+
+  /**
+   * Update pipes position
+   */
+  updatePipes = () => {
+    const { pipes } = this.state;
+    const { mapWidth } = CONF;
+
+    const o = {
+      x: mapWidth,
+      id: Math.random(),
+      height: 570,
+    }
+    return [...pipes, o];
   }
 
   render() {
